1f43b7d43b78855ef4174d9bb443352bcd877952
Tamas <30211285@MB306-STU5298>
2018-10-05 12:12:08 +0100

    aaaaaaa

--- BruteForce/BruteForce/Program.cs
+++ BruteForce/BruteForce/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BruteForce
@@ -10,6 +11,29 @@ namespace BruteForce
     {
         static void Main(string[] args)
         {
+            string password = "77";
+            
+            ///
+            ///Create and combine arrays into one big array
+            ///
+            char[] alphaUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            char[] alphaLowercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToLower().ToCharArray();
+            char[] numbers = "0123456789".ToCharArray();
+            char[] dictionary = alphaLowercase.Concat(alphaUppercase).Concat(numbers).ToArray();
+
+            Console.WriteLine(dictionary.Length);
+
+            int guessLength = 1;
+            char[] guess = null;
+            do
+            {
+                foreach (char dicItem in dictionary)
+                {
+                    guess[1]
+                }
+
+            } while (guess != password);
+                //Console.WriteLine(guess);
         }
     }
 }
